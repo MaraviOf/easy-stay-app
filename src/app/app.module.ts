de9fadc0e4df2ef26ce007aa64509d30dafd47bd
Michel Cruz <130259471+MaraviOf@users.noreply.github.com>
2023-11-14 19:57:02 -0500

    create cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './cliente/header/header.component';
+import { FooterComponent } from './cliente/footer/footer.component';
+import { LandingComponent } from './cliente/landing/landing.component';
+import { FaqComponent } from './cliente/faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
